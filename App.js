9ca9bafd94c162ac39fc8bfa7d621d16e43e05b8
VoChinhHieu <vochinhhieu11082004@gmail.com>
2024-12-15 14:01:38 +0700

    xử lý phần hiện thông tin trong trang cá nhân cũng như phần lưu phiên đăng nhập và cache thông tin người dùng

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import React, { useState } from "react";
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+import React, {useContext} from "react";
 // LoginScreen
 import LoginScreen from "./Screens/LoginScreen/LoginScreen";
 // RegisterScreen
@@ -25,143 +26,133 @@ import ProfileScreen from "./Screens/Mainscreen/ProfileScreen";
 import VideoScreen from "./Screens/Mainscreen/VideoScreen";
 
 // import TopTabNavigator from "./navigation/TopTabNavigator";
-import { Button, View } from "react-native";
-
-import { RegisterProvider } from "./context/RegisterContext";
-import { UserProvider, UserContext } from "./context/UserContext";
+import {RegisterProvider} from "./context/RegisterContext";
+import {UserContext, UserProvider} from "./context/UserContext";
+import {ActivityIndicator} from "react-native";
 
 const authStack = createStackNavigator();
 const profileStack = createStackNavigator();
 const mainStack = createStackNavigator();
 
 const AuthStack = () => {
-  return (
-    <authStack.Navigator screenOptions={{ headerShown: false }}>
-      <authStack.Screen name="Login" component={LoginScreen} />
-      <authStack.Screen
-        name="Register_Step1"
-        component={RegisterScreen_Step1}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step2"
-        component={RegisterScreen_Step2}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step3"
-        component={RegisterScreen_Step3}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step4"
-        component={RegisterScreen_Step4}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step5"
-        component={RegisterScreen_Step5}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step6"
-        component={RegisterScreen_Step6}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step7"
-        component={RegisterScreen_Step7}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step8"
-        component={RegisterScreen_Step8}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step9"
-        component={RegisterScreen_Step9}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <authStack.Screen
-        name="Register_Step10"
-        component={RegisterScreen_Step10}
-        options={{
-          headerShown: false,
-        }}
-      />
-    </authStack.Navigator>
-  );
+    return (
+        <authStack.Navigator screenOptions={{headerShown: false}}>
+            <authStack.Screen name="Login" component={LoginScreen}/>
+            <authStack.Screen
+                name="Register_Step1"
+                component={RegisterScreen_Step1}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step2"
+                component={RegisterScreen_Step2}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step3"
+                component={RegisterScreen_Step3}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step4"
+                component={RegisterScreen_Step4}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step5"
+                component={RegisterScreen_Step5}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step6"
+                component={RegisterScreen_Step6}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step7"
+                component={RegisterScreen_Step7}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step8"
+                component={RegisterScreen_Step8}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step9"
+                component={RegisterScreen_Step9}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <authStack.Screen
+                name="Register_Step10"
+                component={RegisterScreen_Step10}
+                options={{
+                    headerShown: false,
+                }}
+            />
+        </authStack.Navigator>
+    );
+};
+
+const ProfileStack = () => {
+    return (
+        <profileStack.Navigator screenOptions={{headerShown: false}}>
+            <profileStack.Screen name="ProfileScreen" component={ProfileScreen}/>
+            <profileStack.Screen name="FriendList" component={FriendListScreen}/>
+            <profileStack.Screen name="EditProfile" component={EditProfileScreen}/>
+            <profileStack.Screen name="PostArticles" component={PostArticlesScreen}/>
+        </profileStack.Navigator>
+    );
 };
 
 const MainStack = () => {
-  return (
-    <mainStack.Navigator screenOptions={{ headerShown: false }}>
-      <mainStack.Screen name="Home" component={HomeScreen} />
-      <mainStack.Screen name="Post" component={PostScreen} />
-      <mainStack.Screen name="Video" component={VideoScreen} />
-      <mainStack.Screen name="Profile" component={ProfileStack} />
-    </mainStack.Navigator>
-  );
+    return (
+        <mainStack.Navigator screenOptions={{headerShown: false}}>
+            <mainStack.Screen name="Home" component={HomeScreen}/>
+            <mainStack.Screen name="Post" component={PostScreen}/>
+            <mainStack.Screen name="Video" component={VideoScreen}/>
+            <mainStack.Screen name="Profile" component={ProfileStack}/>
+        </mainStack.Navigator>
+    );
 };
 
 const MainApp = () => {
-  const { isAuthenticated } = useContext(UserContext);
-const ProfileStack  = () => {
-  return (
-    <profileStack.Navigator screenOptions={{ headerShown: false }}>
-      <profileStack.Screen name="ProfileScreen" component={ProfileScreen} />
-      <profileStack.Screen name="FriendList" component={FriendListScreen} />
-      <profileStack.Screen name="EditProfile" component={EditProfileScreen} />
-      <profileStack.Screen name="PostArticles" component={PostArticlesScreen} />
-    </profileStack.Navigator>
-  );
+    const {isAuthenticated, isLoading} = useContext(UserContext);
+
+    if (isLoading) {
+        <ActivityIndicator size="large" />
+    }
+
+    return (
+        <NavigationContainer>
+            {isAuthenticated ? <MainStack/> : <AuthStack/>}
+        </NavigationContainer>
+    );
 };
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Thêm setIsAuthenticated vào global để có thể truy cập từ RegisterScreen_Step10
-  global.setIsAuthenticated = setIsAuthenticated;
-
-  return (
-    <NavigationContainer>
-      {isAuthenticated ? <MainStack /> : <AuthStack />}
-      {/* Dùng để debug*/}
-
-      {/* <View style={{ position: "absolute", bottom: 50, right: 20 }}>
-        <Button
-          title="Set Authenticated"
-          onPress={() => setIsAuthenticated(!isAuthenticated)}
-        />
-      </View> */}
-    </NavigationContainer>
-  );
+    return (
+        <UserProvider>
+            <RegisterProvider>
+                <MainApp/>
+            </RegisterProvider>
+        </UserProvider>
+    );
 };
-
-export default function App() {
-  return (
-    <UserProvider>
-      <RegisterProvider>
-        <MainApp />
-      </RegisterProvider>
-    </UserProvider>
-  );
-}
